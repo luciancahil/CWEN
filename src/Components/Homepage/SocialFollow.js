3b05f5c178aa5ab3c9abd545f2a06779f2c2f970
Roy He <royhe62@yahoo.ca>
2021-05-09 02:17:05 -0700

    added social bar

---
+++ src/Components/Homepage/SocialFollow.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faFacebook,
+    faTwitter,
+    faInstagram,
+    faLinkedin
+  } from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+  return (
+    <div class="social-container">
+        <a href="#" className="youtube social">
+            <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a href="#" className="facebook social">
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="#" className="twitter social">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a href="#" className="instagram social">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="#" className="instagram social">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+</div>
+  );
+}
--- src/Components/Homepage/footer.component.js
+++ src/Components/Homepage/footer.component.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import SocialFollow from "./SocialFollow"
 
 class Footer extends React.Component { 
     render() {
-        return <h2>Footer Component</h2>;
+        return (
+            <div id = "footer">
+                <SocialFollow />
+            </div>
+        );
     }
 }
 
