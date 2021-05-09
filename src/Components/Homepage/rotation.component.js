f0b847add8d189dfb87dd7fd6348b8554d0a154f
Roy He <royhe62@yahoo.ca>
2021-05-08 23:54:12 -0700

    added animation end hack

--- src/Components/Homepage/rotation.component.js
+++ src/Components/Homepage/rotation.component.js
@@ -32,17 +32,66 @@ class Rotation extends React.Component {
 
         this.moveLeft = this.moveLeft.bind(this);
         this.moveRight = this.moveRight.bind(this);
+        this.doneAnimation = this.doneAnimation.bind(this);
+        this.finishedMovingLeft = this.finishedMovingLeft.bind(this);
     }
 
     moveLeft(e) {
         e.preventDefault();
         this.setState({sliding: "L"})
+        setTimeout(this.doneAnimation,2000);
     };
 
     moveRight(e) {
         e.preventDefault();
-        this.setState({sliding: "R"})
+        this.setState({sliding: "R"});
+        setTimeout(this.doneAnimation,2000);
     };
+
+    test(){
+        alert("Done!")
+    }
+
+    doneAnimation(){
+        if(this.state.sliding === "L"){
+            alert("Done Moving Left");
+        }else if(this.state.sliding === "R"){
+            alert("Done Moving Right");
+        }else{
+            alert("Neither");
+        }
+    }
+
+    finishedMovingLeft(){
+
+        /*updating the parts Index  array*/
+        let updatingPartsIndex = this.state.partsIndex.slice();
+
+        for(let i = 0; i < 5; i++){
+            updatingPartsIndex[i] = updatingPartsIndex[i + 1];
+        }
+
+        if(updatingPartsIndex[5] === this.state.length - 1){
+            updatingPartsIndex[5] = 0;
+        }else{
+            updatingPartsIndex[5] = updatingPartsIndex[5] - 1;
+        }
+
+        /* building the state parts array */
+        let buildingStateParts = [];
+        for(let i = 0; i < 5; i++){
+            console.log(updatingPartsIndex[i])
+            buildingStateParts[i] = this.props.parts[updatingPartsIndex[i]];
+        }
+
+
+        this.setState({
+            sliding: "N",
+            partsIndex: updatingPartsIndex,
+            parts: buildingStateParts
+        })
+
+    }
     
 
     render() {
@@ -67,7 +116,7 @@ class Rotation extends React.Component {
 
                         <div id = "rotatingWrapper">
                             <div id = "rotating">
-                                {this.state.parts.map(stuff => <Rotpart key = {stuff.heading} parts = {stuff} sliding = {this.state.sliding}/>)}
+                                {this.state.parts.map(stuff => <Rotpart onAnimationEnd = {this.doneAnimation} key = {stuff.heading} parts = {stuff} sliding = {this.state.sliding}/>)}
                             </div>
                         </div>
                     </div>
