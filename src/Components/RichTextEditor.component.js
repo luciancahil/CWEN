0243889b1e8bdc7d3e1a18f39e74c5234a1d08e7
Roy He <royhe62@yahoo.ca>
2021-06-06 23:49:31 -0700

    changed text editor again

--- src/Components/RichTextEditor.component.js
+++ src/Components/RichTextEditor.component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+
 
 
 
@@ -9,27 +10,45 @@ class RichTextEditor extends React.Component {
       super(props);
     }
 
+
     render() {
       return (
         <div className="App">
-                <h2>Using CKEditor 5 build in React</h2>
+                <h2>CKEditor 5 using a custom build - decoupled editor</h2>
                 <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
                     onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
                         console.log( 'Editor is ready to use!', editor );
+
+                        // Insert the toolbar before the editable area.
+                        editor.ui.getEditableElement().parentElement.insertBefore(
+                            editor.ui.view.toolbar.element,
+                            editor.ui.getEditableElement()
+                        );
+
+                        this.editor = editor;
+                    } }
+                    onError={ ( { willEditorRestart } ) => {
+                        // If the editor is restarted, the toolbar element will be created once again.
+                        // The `onReady` callback will be called again and the new toolbar will be added.
+                        // This is why you need to remove the older toolbar.
+                        if ( willEditorRestart ) {
+                            this.editor.ui.view.toolbar.element.remove();
+                        }
                     } }
                     onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
+                      const data = editor.getData();
+                      console.log( { data } );
+                  } }
+                    editor={ DecoupledEditor }
+                    data="<p>Hello from CKEditor 5's decoupled editor!</p>"
+                    config = {
+                        [
+                          ['Font','FontSize'],
+                          ['Bold','Italic','Underline'],
+                          ['TextColor','BGColor'],
+                          ['JustifyLeft', 'JustifyCenter', 'JustifyRight']
+                      ]
+                  }
                 />
             </div>
       );
