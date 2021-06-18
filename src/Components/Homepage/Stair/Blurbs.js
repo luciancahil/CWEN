9b6e9f2c31393da5d418a0f9e2343f00fa762dda
Roy He <royhe62@yahoo.ca>
2021-06-18 05:09:25 -0700

    fixed staircase typos

--- src/Components/Homepage/Stair/Blurbs.js
+++ src/Components/Homepage/Stair/Blurbs.js
@@ -1,9 +1,9 @@
 const Blurbs ={
-    "Shelf":"The main aim is to support high potential women entrepreneurs gain shelf space in leading local and regional stores. By strengthening the capacity of women food entrepreneurs and other products through value addition, packaging, branding, marketing, merchandising and promotion.",
-    "Advocacy":"Expanding women’s economic opportunities is thus more about tackling constraints to women’s abilities and incentives to expand their business and moving into higher value added activities—than it is about expanding entrepreneurship in itself. Work we work towards creating and enabling environment for women entrepreneurs to thrive. Research, advocacy are key instruments.",
-    "Bootcamps":"In an increasingly interdependent world, entrepreneurship provides a powerful platform to connect and build pathways to peace. Through entrepreneurship, we can get young women to work together to tackle some of Uganda’s complex problems, while increasing continental prosperity.",
+    "Shelf":"The main aim is to support high potential women entrepreneurs to gain shelf space in leading local and regional stores. By strengthening the capacity of women food entrepreneurs and other products through co-shared processing and production facility, packaging, branding, marketing, merchandising and promotion.",
+    "Advocacy":" Expanding women’s economic opportunities is thus more about tackling constraints to women’s abilities and incentives to expand their business and moving into higher value added activities—than it is about expanding entrepreneurship in itself. We work towards creating an enabling environment for women entrepreneurs to thrive. Research and advocacy are key instruments.",
+    "Bootcamps":"In an increasingly interdependent world, entrepreneurship provides a powerful platform to connect and build pathways to peace. Through entrepreneurship, young women to work together to tackle some of Uganda’s complex problems, while increasing continental prosperity.",
     "Events":"Our events are a big hit. From small intimate meetups of CWen meets to the annual Women Entrepreneurs’ Social. We strive to bring women entrepreneurs together to harness the power of networking and joint venture partnerships.",
-    "Month":"Every month, we shall feature a woman entrepreneur sharing her journey, products and services."
+    "Month":"Every month we feature a woman entrepreneur sharing her journey, products and services plus distribution channels."
  }
 
  export default Blurbs;
