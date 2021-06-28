7af52f5ea48d5a81fd06a6056246e6dcc92d6ffe
Roy He <royhe62@yahoo.ca>
2021-06-28 14:26:52 -0700

    added 404 page

---
+++ src/Components/404.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Four04 extends React.Component { 
+    render() {
+        return (
+            <div id = "404">
+                <h2>Sorry! What you're looking for has move</h2>
+                <p>Would you like to <a href = "/">go home</a>?</p>
+            </div>
+        );
+    }
+}
+
+export default Four04;
--- src/Components/project.component.js
+++ src/Components/project.component.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 class Project extends React.Component { 
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          slogan: "CWEN was built by women, for women.The buisness world was not."
+        };
+    }
+
+    componentDidMount(){
+        let query = this.props.location.search;
+        let projectNameIndex = query.indexOf("projectName=");
+
+        
+    }
+
     render() {
+        // the query of the url
+
         return <h2>Project Page</h2>;
     }
 }
