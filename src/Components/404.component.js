b471f005d5d58132bcfb622d339a2e1e3cefcb96
Roy He <royhe62@yahoo.ca>
2021-06-28 18:30:29 -0700

    fixed type in 404

--- src/Components/404.component.js
+++ src/Components/404.component.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Four04 extends React.Component { 
     render() {
         return (
-            <div id = "404">
-                <h2>Sorry! What you're looking for has moved</h2>
+            <div id = "fourohfour">
+                <h2>Sorry! What you're looking for has moved or doesn't exist</h2>
                 <p>Would you like to <a href = "/">go home</a>?</p>
             </div>
         );
