56e0595300223bf3c1b49b2ae60f15f1007f4d30
Roy He <royhe62@yahoo.ca>
2021-07-05 19:32:26 -0700

    added month constructor

--- src/Components/month.component.js
+++ src/Components/month.component.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Month extends React.Component { 
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          name: "start",
+          buisiness: "",
+          products: []
+        };
+    }
+
+    componentDidMount(){
+
+    }
+
     render() {
         return <h2>Month Page</h2>;
     }
