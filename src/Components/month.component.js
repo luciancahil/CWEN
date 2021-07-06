d12c5883971b44ac03043f1f7534c974b486a81a
Roy He <royhe62@yahoo.ca>
2021-07-05 19:23:34 -0700

    added entrepreur of the month component

---
+++ src/Components/month.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Month extends React.Component { 
+    render() {
+        return <h2>Month Page</h2>;
+    }
+}
+
+export default Month;
--- src/Components/project.component.js
+++ src/Components/project.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Four04 from './404.component';
+import Month from './month.component';
 
 class Project extends React.Component { 
 
@@ -53,8 +54,10 @@ class Project extends React.Component {
     render() {
         // true if we are on the events project
         let isOnEvents = this.state.projectName === "CWEN Events";
-        console.log("url: " + this.state.projectName);
-        if(this.state.url === "404"){
+        console.log("url: " + (this.state.projectName === "Woman Entrepreneur of the Month"));
+        if(this.state.projectName === "Woman Entrepreneur of the Month"){
+            return <Month/>
+        }else if(this.state.url === "404"){
             return <Four04/>;
         }else if(this.state.url === "start"){
             return <p id = "loading">loading...</p>
