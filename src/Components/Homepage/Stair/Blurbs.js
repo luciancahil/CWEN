287e7165682c795da0e415867b293986cc908a6b
Roy He <royhe62@yahoo.ca>
2021-07-16 13:29:25 -0700

    fixed typo

--- src/Components/Homepage/Stair/Blurbs.js
+++ src/Components/Homepage/Stair/Blurbs.js
@@ -1,7 +1,7 @@
 const Blurbs ={
     "Shelf":"The main aim is to support high potential women entrepreneurs to gain shelf space in leading local and regional stores. By strengthening the capacity of women food entrepreneurs and other products through co-shared processing and production facility, packaging, branding, marketing, merchandising and promotion.",
     "Advocacy":"Expanding women’s economic opportunities is thus more about tackling constraints to women’s abilities and incentives to expand their business and moving into higher value added activities—than it is about expanding entrepreneurship in itself.",
-    "Bootcamps":"In an increasingly interdependent world, entrepreneurship provides a powerful platform to connect and build pathways to peace. Through entrepreneurship, young women to work together to tackle some of Uganda’s complex problems, while increasing continental prosperity.",
+    "Bootcamps":"In an increasingly interdependent world, entrepreneurship provides a powerful platform to connect and build pathways to peace. Through entrepreneurship, young women work together to tackle some of Uganda’s complex problems, while increasing continental prosperity.",
     "Events":"Our events are a big hit. From small intimate meetups to the annual Women Entrepreneurs’ Social. We strive to bring women entrepreneurs together to harness the power of networking and joint venture partnerships.",
     "Month":"Every month we feature a woman entrepreneur sharing her journey, products and services plus distribution channels."
  }
