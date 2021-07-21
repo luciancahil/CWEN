7379c628ae0c3700d6abc2f01f2c68584245ed9a
Roy He <royhe62@yahoo.ca>
2021-07-21 14:16:21 -0700

    added a bit of login documentation

--- src/Components/login.component.js
+++ src/Components/login.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class Login extends React.Component { 
+    // this is the box for both loging in and signing up
     render() {
         return <h2>Login Page</h2>;
     }
