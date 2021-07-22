9dbcf3380f2fb0e142a7f5b2d26b78af6b0c6198
Roy He <royhe62@yahoo.ca>
2021-07-21 21:20:54 -0700

    added styling to button

--- src/Components/resetRequest.component.js
+++ src/Components/resetRequest.component.js
@@ -14,7 +14,6 @@ class ResetRequest extends React.Component {
 
     onChangeEmail(e){
         e.preventDefault();
-
         this.setState({
             email: e.target.value
         })
@@ -29,37 +28,8 @@ class ResetRequest extends React.Component {
         fetch(fetchURL)
             .then((response) => response.text())
             .then((text) => {
-               // console.log(text)
-                
-                if(text === "unfound"){
-                    this.setState({
-                        login_status: "That username is not registered. Would you like to sign up?"
-                    })
-                }else if(text === "incorrect"){
-                    this.setState({
-                        login_status: "That password is incorrect. Would you like to reset your password?"
-                    })
-                }else if(text === "err"){
-                    this.setState({
-                        login_status: "Server Error! Please try again."
-                    })
-                }else{
-                    // everything before this is the type, everything after this is the token
-                    let cutoff = text.indexOf(",");
-
-                    let title = text.substr(0,cutoff);
-                    
-                    let token = text.substr(cutoff + 1);
-
-                    this.setState({
-                        login_status: ""
-                    })
-
-                    localStorage.setItem("title", title);
-                    localStorage.setItem("token", token);
-
-                    window.location.href = "/logged_in"
-                }
+                console.log(fetchURL);
+                console.log(text);
             })
     }
 
@@ -75,7 +45,7 @@ class ResetRequest extends React.Component {
                             <plaintext>Email:</plaintext><input type = "text" value = {this.state.login_username} onChange = {this.onChangeEmail}></input><br></br>                            
                         </div>
                         <div id = "boxButtons">
-                            <div className = "LoginButtonOuter">
+                            <div className = "LoginButtonOuter" id = "ResetRequestButtons">
                                 <div className = "LoginButtonInner">
                                     <button onClick = {this.onSubmit}>Get Reset Link</button>
                                 </div>
