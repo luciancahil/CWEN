4416fe5b9cad99084d70b5a287e09593b63fe025
Roy He <royhe62@yahoo.ca>
2021-08-01 00:37:47 -0700

    added ability to read query on Services

--- src/Components/footer.component.js
+++ src/Components/footer.component.js
@@ -21,11 +21,13 @@ class Footer extends React.Component {
                     <div className = "footPart">
                         <h3>Our Services</h3>
                         <ul>
-                            <li><a href = "/Services?type=Networking">Networking</a></li>
-                            <li><a href = "/Services?type=Sales+And+Marketing">Sales and Marketing</a></li>
-                            <li><a href = "/Services?type=Buisness+Consultations">Business Consultations</a></li>
+                            <li><a href = "/Services?type=Co-Production+and+Processing+Facility">Co-Production and Processing Facility</a></li>
+                            <li><a href = "/Services?type=Contract+Packaging">Contract Packaging</a></li>
                             <li><a href = "/Services?type=Packaging+And+Branding">Packaging and Branding</a></li>
+                            <li><a href = "/Services?type=Buisness+Consultations">Business Consultations</a></li>
+                            <li><a href = "/Services?type=Sales+And+Marketing">Sales and Marketing</a></li>
                             <li><a href = "/Services?type=Technical+Services">Technical Services</a></li>
+                            <li><a href = "/Services?type=Networking+And+Events">Networking and Events</a></li>
                         </ul>
                     </div>
 
--- src/Components/services.component.js
+++ src/Components/services.component.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import ServiceDesciptions from './serviceDesciptions';
 
 class Services extends React.Component {
+  constructor(props){
+    super(props);
+
+    let query = this.props.location.search;
+    let start = "type="
+    let projectNameIndex = query.indexOf(start);
+    let type = query.substring(projectNameIndex + start.length).replaceAll("+", " ");
+    
+  }
+
   componentDidMount() {
     document.title = 'CWEN - Services';
   }
