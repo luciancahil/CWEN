e19b6102ed3756b1c35c396e5ee3aebb86db82c9
Roy He <royhe62@yahoo.ca>
2021-08-01 00:58:45 -0700

    added imforation to services pages

--- src/Components/services.component.js
+++ src/Components/services.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ServiceDesciptions from './serviceDesciptions';
+import Four04 from './404.component';
 
 class Services extends React.Component {
   constructor(props){
@@ -9,7 +10,9 @@ class Services extends React.Component {
     let start = "type="
     let projectNameIndex = query.indexOf(start);
     let type = query.substring(projectNameIndex + start.length).replaceAll("+", " ");
-    
+    this.state = {
+      serviceType: type
+    }
   }
 
   componentDidMount() {
@@ -17,7 +20,19 @@ class Services extends React.Component {
   }
   
   render() {
-    return <h2>Services Page</h2>;
+    let content = ServiceDesciptions[this.state.serviceType];
+    console.log(this.state.serviceType)
+
+    if(content === undefined){
+      return <Four04/>
+    }
+    
+    return (
+      <div id = "services">
+        <h2>{this.state.serviceType}</h2>
+        <p>{ServiceDesciptions[this.state.serviceType]}</p>
+      </div>
+    );
   }
 }
 
