a8ee1c044c589525c76d71b58a5ad80df01d7b62
Roy He <royhe62@yahoo.ca>
2021-08-12 19:07:36 -0700

    added basic functionality

--- src/Components/join.component.js
+++ src/Components/join.component.js
@@ -1,8 +1,135 @@
 import React from 'react';
 
 class Join extends React.Component { 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            Name: "",
+            Phone: "",
+            Email: "",
+            Buisness: "",
+            Description: "",
+            Region: "",
+            District: "",
+            Town: ""
+        }
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangePhone = this.onChangePhone.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangeBuisness = this.onChangeBuisness.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeRegion = this.onChangeRegion.bind(this);
+        this.onChangeDistrict = this.onChangeDistrict.bind(this);
+        this.onChangeTown = this.onChangeTown.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChangeName(e){
+        e.preventDefault();
+
+        
+    
+        this.setState({
+            Name: e.target.value
+        })
+        
+    }
+
+    onChangePhone(e){
+        e.preventDefault();
+
+        this.setState({
+            Phone: e.target.value
+        })
+        
+    }
+
+    onChangeEmail(e){
+        e.preventDefault();
+
+        this.setState({
+            Email: e.target.value
+        })
+        
+    }
+
+    onChangeBuisness(e){
+        e.preventDefault();
+
+        this.setState({
+            Buisness: e.target.value
+        })
+        
+    }
+    
+    onChangeDescription(e){
+        e.preventDefault();
+
+        this.setState({
+            Description: e.target.value
+        })
+        
+    }
+
+    onChangeRegion(e){
+        e.preventDefault();
+
+        this.setState({
+            Region: e.target.value
+        })
+        
+    }
+
+    onChangeDistrict(e){
+        e.preventDefault();
+
+        this.setState({
+            District: e.target.value
+        })
+        
+    }
+
+    onChangeTown(e){
+        e.preventDefault();
+
+        this.setState({
+            Town: e.target.value
+        })
+        
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        console.log(this.state);
+    }
+
     render() {
-        return <h2>Join Page</h2>;
+        return (
+            <div id = "joinPage">
+                <h3>Name</h3>
+                <input type = "text" id = "Name" value={this.state.Name} onChange={this.onChangeName}/>
+                <h3>Phone Number</h3>
+                <input type = "text" id = "Phone" value={this.state.Phone} onChange={this.onChangePhone}/>
+                <h3>Email</h3>
+                <input type = "text" id = "Email" value={this.state.Email} onChange={this.onChangeEmail}/>
+                <h3>Buisness Name</h3>
+                <input type = "text" id = "Buisness" value={this.state.Buisness} onChange={this.onChangeBuisness}/>
+                <h3>Tell us about your buissness!</h3>
+                <input type = "text" id = "descrption" value={this.state.Description} onChange={this.onChangeDescription}/>
+                <h3>Region</h3>
+                <input type = "text" id = "Region" value={this.state.Region} onChange={this.onChangeRegion}/>
+                <h3>District</h3>
+                <input type = "text" id = "District" value={this.state.District} onChange={this.onChangeDistrict}/>
+                <h3>Town</h3>
+                <input type = "text" id = "Town" value={this.state.Town} onChange={this.onChangeTown}/>
+                <br/> <br/> <br/>
+                <button onClick = {this.onSubmit}>Join!</button>
+            </div>
+
+        );
     }
 }
 
