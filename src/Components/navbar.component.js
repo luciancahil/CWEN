ab980c574adf609c9a761e9820bdf59a8b947a18
Roy He <royhe62@yahoo.ca>
2021-08-16 20:00:46 -0700

    added automatic resizing check in navbar

--- src/Components/navbar.component.js
+++ src/Components/navbar.component.js
@@ -2,7 +2,35 @@ import React from 'react';
 import logo from './logo.jpg';
 
 class Navbar extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      width: window.innerWidth
+    }
+
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions(){
+    this.setState({
+      width: window.innerWidth
+    })
+  }
+
   render() {
+
+    if(this.state.width <= 590){
+      return null;
+    }
+
+    
+
+
     return (
       <nav>
         <a href = "/"><img alt = "logo" src = {logo}/></a>
