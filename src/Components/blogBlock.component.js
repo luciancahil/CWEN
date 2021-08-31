859eefd6015bf10b86d3943faacbd429a76bc1e5
Roy He <royhe62@yahoo.ca>
2021-08-30 22:19:39 -0700

    added sizing

--- src/Components/blogBlock.component.js
+++ src/Components/blogBlock.component.js
@@ -15,7 +15,8 @@ class BlogBlock extends React.Component {
 
         if(this.props.block.imgID !== undefined){
             console.log(this.props.block.entityMap);
-            return <img src = {this.props.imageArray[this.props.block.imgID]} alt = "not found"/>
+            let style = "height:" + this.props.block.entityMap.data.height + ";width:" + this.props.block.entityMap.data.width;
+            return <img style = {{height: [this.props.block.entityMap.data.height]}, {width: [this.props.block.entityMap.data.width]}} src = {this.props.imageArray[this.props.block.imgID]} alt = "not found"/>
         }
 
         if(this.props.block.text === ""){
