07e336e5fe28173f3a1f90a68f17dc1b21266ad3
Roy He <royhe62@yahoo.ca>
2021-08-30 20:49:38 -0700

    parsing query

--- src/Components/blog.component.js
+++ src/Components/blog.component.js
@@ -4,6 +4,25 @@ import Four04 from './404.component';
 class Blog extends React.Component {
   componentDidMount() {
     document.title = 'CWEN Blog';
+    let query = this.props.location.search;
+    let baseURL = "http://localhost:4000/"
+    let id = ""
+    let author = ""
+
+
+    // getting the name and ID
+    let authorIndex = query.indexOf("author=");
+    let ampIndex = query.indexOf("&");
+    let idIndex = query.indexOf("id=")
+    id = query.substring(idIndex + "id=".length);
+    author = query.substring(authorIndex + "author=".length, ampIndex);
+    console.log("author: " + author);
+    console.log("id: " + id);
+
+    let contentURL = baseURL + ""
+
+
+    console.log(query);
   }
 
   render() {
