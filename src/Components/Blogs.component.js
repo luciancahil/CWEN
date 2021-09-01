30781c4c2bbbe729f08f8ce68bfc00d864cbdfcf
Roy He <royhe62@yahoo.ca>
2021-09-01 07:10:37 -0700

    fetched in blog page

--- src/Components/Blogs.component.js
+++ src/Components/Blogs.component.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import SelfDisplay from './selfDisplay.component';
 
-class Blogs extends React.Component { 
+class Blogs extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            blogInfo: [],
+            searchField: ""
+        }
+    }
+
+    componentDidMount(){
+        let URL =  "https://cwen-backend.herokuapp.com/recentBlogs"
+
+
+        fetch(URL)
+            .then((response) => response.json())
+            .then((content) => this.setState({blogInfo: content}));
+    }
+    
     render() {
-        return <h2>Blogs Page</h2>;
+        return <div id = "blogs">
+            {this.state.blogInfo.map((blogStuff) => <SelfDisplay blogInfo = {blogStuff} key = {blogStuff}/>)}
+        </div>
     }
 }
 
