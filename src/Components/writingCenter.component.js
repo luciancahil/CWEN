2645f1148ffa7ee4378c25a4d007b789b3949c5f
Roy He <royhe62@yahoo.ca>
2021-09-01 03:00:39 -0700

    finished writing center fetch

--- src/Components/writingCenter.component.js
+++ src/Components/writingCenter.component.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 class WritingCenter extends React.Component { 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            blogInfo: []
+        }
+    }
+
+    componentDidMount(){
+        let URL =  "https://cwen-backend.herokuapp.com/allBlogs?token=" + encodeURI(localStorage.getItem("token")).replaceAll("+","%2B")
+
+        console.log(URL);
+
+        fetch(URL)
+            .then((response) => response.json())
+            .then((content) => this.setState({blogInfo: content}));
+    }
+
     render() {
+        console.log(this.state.blogInfo);
         return <h2>WritingCenter Page</h2>;
     }
 }
