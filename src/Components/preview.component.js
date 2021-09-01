3098113254d25ec9c6908f12e63e1a9e51a90d5e
Roy He <royhe62@yahoo.ca>
2021-09-01 05:46:03 -0700

    added preview page

---
+++ src/Components/preview.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Blog from "./blog.component"
+
+class Preview extends React.Component { 
+    render() {
+        return <h2>Featured Page</h2>;
+    }
+}
+
+export default Preview;
