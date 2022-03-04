7ffe4a0f84ea105002cc632a182f1a50e856ad42
royhe <royhe62@yahoo.ca>
2022-03-03 17:39:29 -0800

    fixed positioning of iframe

--- src/Components/Homepage/Video.component.js
+++ src/Components/Homepage/Video.component.js
@@ -3,9 +3,11 @@ import React from 'react';
 class Video extends React.Component { 
     render() {
         return (
-            <iframe width="420" height="315"
-                src="https://www.youtube.com/embed/od1fL3m302Q">
-            </iframe>
+            <div id = "tv-wrapper">
+                <iframe id = "trailer" width="420" height="315"
+                    src="https://www.youtube.com/embed/od1fL3m302Q">
+                </iframe>
+            </div>
         );
     }
 }
