a7367cae83476446c8fea53b5b4f4d91a538c821
luciancahil <royhe62@yahoo.ca|>
2024-04-27 11:08:30 -0700

    Added Donate Button.

--- src/Components/SocialFollow.js
+++ src/Components/SocialFollow.js
@@ -26,6 +26,28 @@ export default function SocialFollow() {
         <a href="https://www.linkedin.com/company/cwen/" className="linkedin social">
             <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
+        
+        <a 
+  className="dbox-donation-page-button" 
+  href="https://donorbox.org/seeds-of-empowerment-cultivating-change-through-shared-innovation?default_interval=o" 
+  style={{ 
+    background: 'rgb(245, 118, 0)', 
+    color: 'rgb(255, 255, 255)', 
+    textDecoration: 'none', 
+    fontFamily: 'Verdana, sans-serif', 
+    display: 'flex', 
+    fontSize: '16px', 
+    margin: '20px 10px',
+    padding: '8px 24px', 
+    borderRadius: '5px', 
+    gap: '8px', 
+    width: 'fit-content', 
+    lineHeight: '24px'
+  }}
+>
+  <img src="https://donorbox.org/images/white_logo.svg" alt="Donate" />
+  Donate
+</a>
 </div>
   );
 }
